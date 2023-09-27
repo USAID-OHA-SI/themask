98b1bfec279290d859a5fcebd40c34029ce1a16e
Aaron Chafetz <achafetz@usaid.gov>
2023-09-27 17:06:16 -0400

    feat: add uid fcn

---
+++ R/generate_uid.R
@@ -0,0 +1,24 @@
+#' Generate a Unique ID
+#'
+#' This function is used to create a unique ID (UID) to mimic the UIDs created
+#' and used by DATIM for the MER and other PEPFAR Structured Datasets.
+#'
+#' @param codeSize character length for UID output (default = 11)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'  generate_uid()
+
+generate_uid <- function(codeSize=11){
+  #Generate a random seed
+  runif(1)
+  allowedLetters<-c(LETTERS,letters)
+  allowedChars<-c(LETTERS,letters,0:9)
+  #First character must be a letter according to the DHIS2 spec
+  firstChar<-sample(allowedLetters,1)
+  otherChars<-sample(allowedChars,codeSize-1)
+  uid<-paste(c(firstChar,paste(otherChars,sep="",collapse="")),sep="",collapse="")
+  return(uid)
+}
