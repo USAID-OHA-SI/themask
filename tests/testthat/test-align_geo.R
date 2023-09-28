51ec2eade41617ebea537a93cf14119fc1d15e2b
Aaron Chafetz <achafetz@usaid.gov>
2023-09-28 15:49:44 -0400

    test: include test on minoria table structure

--- tests/testthat/test-align_geo.R
+++ tests/testthat/test-align_geo.R
@@ -1,3 +1,8 @@
-# test_that("masked geography retains its structure", {
-#   expect_equal(names(msk_), 4)
-# })
+test_that("masked geography (Minoria) retains its structure", {
+  col_names <- c("operatingunit_milb", "operatingunituid_milb",
+                 "country_milb", "snu1_milb", "snu1uid_milb",
+                 "psnu_milb", "psnuuid_milb")
+
+  expect_equal(names(minoria_geo), col_names)
+  expect_equal(nrow(minoria_geo), 16)
+})
